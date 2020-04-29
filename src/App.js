a1bb517418de2aac405015441686e36074a570cc
Alvaro Fariña <farinalvaro@gmail.com>
2020-04-28 22:44:29 -0300

    added browser router at the root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+    </BrowserRouter>
   );
 }
 
